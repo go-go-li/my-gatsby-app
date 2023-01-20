d7cd3f5178223e438c4bf3b24de7adf238910403
go-go-li <83599414+go-go-li@users.noreply.github.com>
2023-01-20 15:05:37 +0100

    markdowns

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,17 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-  siteMetaData: {
+  plugins: [
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`
+      }
+    }
+  ],
+  siteMetadata: {
     title: "Web Warrior",
     description: "web dev portfolio",
     copyright: "This website is copyright 2023"
